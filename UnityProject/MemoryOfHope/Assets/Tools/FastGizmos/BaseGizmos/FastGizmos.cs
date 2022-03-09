798f4a803cd6f87b4dffdcd8861631acafb07b1d
Tiondel <p.serenrosso@rubika.com>
2022-03-09 17:14:45 +0100

    Architecture Gizmos Tools

---
+++ UnityProject/MemoryOfHope/Assets/Tools/FastGizmos/BaseGizmos/FastGizmos.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class FastGizmos
+{
+ public GizmosType Type;
+ public GizmosParameter.Base Parameter;
+}
---
+++ UnityProject/MemoryOfHope/Assets/Tools/FastGizmos/GizmosProfilsManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GizmosProfilManager", menuName = "GizmosProfilManager", order = 1)]
+public class GizmosProfilsManager : ScriptableObject
+{
+    public List<GizmosProfil> GizmosProfils;
+}
