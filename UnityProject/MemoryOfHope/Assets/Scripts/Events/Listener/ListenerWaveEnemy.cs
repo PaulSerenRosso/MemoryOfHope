7030aff9688f394f86bddec826a1712dff1d4ea4
Tiondel <p.serenrosso@rubika.com>
2022-04-11 11:03:29 +0200

    EventWaveEnemy

--- UnityProject/MemoryOfHope/Assets/Scripts/Events/Listener/ListenerWaveEnemy.cs
+++ UnityProject/MemoryOfHope/Assets/Scripts/Events/Listener/ListenerWaveEnemy.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ListenerWaveEnemy : MonoBehaviour
 {
+    [SerializeField]
+    private List<EnemyManager> _allEnemies;
+    [SerializeField] private List<CheckIndexEnemiesList> checkEnemyCount;
+
     // Start is called before the first frame update
-    void Start()
+    void OnValidate()
     {
+        for (int i = 0; i < _allEnemies.Count; i++)
+        {
+            _allEnemies[i].WaveListener = this;
+        }
+    }
+    public void Raise(EnemyManager enemy)
+    {
+        for (int i = 0; i < checkEnemyCount.Count; i++)
+        {
+
+            if (_allEnemies.Count == checkEnemyCount[i].Index)
+            {
+          
+                checkEnemyCount[i]?.Event.Invoke();   
+                checkEnemyCount.Remove(checkEnemyCount[i]); 
+                break;
+            }
+          
+        }
+        enemy.WaveListener = null;
+        _allEnemies.Remove(enemy);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public class CheckIndexEnemiesList
     {
-        
+        public int Index;
+        public UnityEvent Event;
     }
+    
+ 
 }
