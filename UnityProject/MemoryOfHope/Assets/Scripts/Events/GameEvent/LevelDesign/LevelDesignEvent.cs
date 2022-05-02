8c38495ab2a2e39b98877a2e59c274b3c10c27c8
Baudinet Gauthier <80094955+BGauthier59@users.noreply.github.com>
2022-05-02 02:21:45 +0200

    Maj jungle door & material instance

---
+++ UnityProject/MemoryOfHope/Assets/Scripts/Events/GameEvent/LevelDesign/LevelDesignEvent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "Event/GameEvent/Tutorial/LevelDesign", order = 2)]
+public class LevelDesignEvent : ScriptableObject
+{
+    public void OpeningDoor(GameObject door)
+    {
+        door.SetActive(false);
+    }
+
+    public void ActivateLaser()
+    {
+        
+    }
+}
