f668bdec5eefdd8d03233114c18af46350a6eb3f
Baudinet Gauthier <80094955+BGauthier59@users.noreply.github.com>
2022-05-25 15:42:10 +0200

    Battery feedback polish & door break polish

--- UnityProject/MemoryOfHope/Assets/Scripts/LevelDesigner/LaserScripts/DoorActivator.cs
+++ UnityProject/MemoryOfHope/Assets/Scripts/LevelDesigner/LaserScripts/DoorActivator.cs
@@ -5,10 +5,13 @@ public class DoorActivator : MonoBehaviour, IReturnable
 {
     [SerializeField] private DoorLaserMultiple currentDoor;
 
+    public UnityEvent ActiveEvent;
+    public UnityEvent DeactiveEvent;
+
+    public Renderer[] glasses;
+    public Color glassColorDeactivate;
     
-  public UnityEvent ActiveEvent;
-  public UnityEvent DeactiveEvent;
-  public virtual bool IsReturnLaser
+    public virtual bool IsReturnLaser
     {
         get { return _triggerByLaser; }
         set { _triggerByLaser = value; }
@@ -47,6 +50,7 @@ public class DoorActivator : MonoBehaviour, IReturnable
         if (currentDoor.IsActive)
         {
             particles.Stop();
+            ChangeColor(glassColorDeactivate);
             currentDoor.ActivedActivatorsCount--;
             DeactiveEvent?.Invoke();
             _triggerByLaser = false;
@@ -59,10 +63,21 @@ public class DoorActivator : MonoBehaviour, IReturnable
         if (currentDoor.IsActive)
         {
             particles.Play();
+            ChangeColor(Color.white);
             currentDoor.ActivedActivatorsCount++;
             _triggerByLaser = true;
             _currentSource = laser;
             currentDoor.CheckActivator();
+        }
+    }
+
+    public void ChangeColor(Color color)
+    {
+        foreach (var renderer in glasses)
+        {
+            renderer.sharedMaterials[1].GetColor("BatteryColor");
+            renderer.sharedMaterials[1].SetColor("BatteryColor", color);
+
         }
     }
 }
